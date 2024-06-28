a079a5721a8ed7307b3992894398c0e92604319c
bauyrzhan <newnewernewest123@gmail.com>
2024-06-28 16:46:41 +0500

    html

--- mycrm/urls.py
+++ mycrm/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', home, name= 'home'),
+    path('login/', login_user, name= 'loginpage'),
+    path('logout/', logout_user, name= 'logout'),
+
 ]
